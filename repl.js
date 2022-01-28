82563fd99cd8afccfc893c2da1f7f247f16e2809
ixc7 <isaacxpreston7@gmail.com>
2022-01-28 14:10:45 -0500

    remove unused vars, add clear option

--- repl.js
+++ repl.js
@@ -3,17 +3,9 @@
 const { createInterface } = require('readline')
 
 let stack = []
-let lastValid = ''
 
 const quit = (code = 0) => process.exit(code)
 const validNum = n => !isNaN(parseFloat(n)) && isFinite(n)
-const x1b = (msg, color = 1) => `\x1b[1m\x1b[38;5;${color}m${msg}\x1b[0m`
-
-const cursor = '| '
-const divider = '------------------'
-const commandsMsg = `commands: ${x1b('q', 49)}(uit), ${x1b('r', 51)}(eset), ${x1b('h', 57)}(elp)\n`
-const initMsg = `\n${divider}\n${x1b('rpn calc v0.0.1', 128)}\n${divider}\n${commandsMsg}\n`
-
 const operators = {
   '+': (a, b) => a + b,
   '-': (a, b) => a - b,
@@ -21,10 +13,13 @@ const operators = {
   '/': (a, b) => a / b
 }
 
-const error = (msg = 'error') => { 
-  console.log(x1b(msg))
-  return false
-}
+const cursor = '| '
+const divider = '------------------'
+const x1b = (msg, color = 1) => `\x1b[1m\x1b[38;5;${color}m${msg}\x1b[0m`
+const commandsMsg = `commands: ${x1b('q', 49)}(uit), ${x1b('r', 124)}(eset), ${x1b('c', 46)}(lear), ${x1b('h', 57)}(elp)`
+const initMsg = `\n${divider}\n${x1b('rpn calc v0.0.1', 128)}\n${divider}\n${commandsMsg}\n\n`
+
+const error = (msg = 'error') => console.log(x1b(msg))
 
 const evaluate = input => {
   const expr = input.split(' ').filter(i => i.length && i !== ' ')
@@ -36,7 +31,6 @@ const evaluate = input => {
     const isOpr = operators[char]
 
     if (!isNum && !isOpr) return error(`Invalid syntax: ${char}`)
-        
     if (isNum) stack.push(parseFloat(char))
 
     else if (isOpr) {
@@ -46,30 +40,21 @@ const evaluate = input => {
         const a = localStack.pop()
         const calculated = operators[char](a, b)
         if (!validNum(calculated)) return error(`Illegal operation: ${a} ${char} ${b}`)
+        
         localStack.push(calculated)
         stack = localStack  
       }
-      
       else if (stack.length === 1) {
-        lastValid = stack
-        let msg = `${x1b('stack has one item:', 8)} ${x1b(lastValid[0], 6)}`
+        let msg = `${x1b('Stack has one item:', 8)} ${x1b(stack[0], 6)}`
         if (i > 0) msg += `\ncould not evaluate ${x1b(len - i, 6)} out of ${x1b(len, 6)} arguments passed`
         return error(msg)
       }
-      
-      else if (stack.length < 1) {
-        let msg = x1b('Stack is empty', 8)
-        if (lastValid) msg += `${x1b(' last result:', 8)} ${x1b(lastValid[0], 6)}`
-        return error(msg)
-      }
+      else if (stack.length < 1) return error(x1b('Stack is empty', 8))
     }
   }
 
-  lastValid = stack
-  return true
+  return console.log(`${cursor}${x1b(stack, 4)}`)
 }
-
-//////////
 
 const rl = createInterface({
   input: process.stdin,
@@ -78,31 +63,33 @@ const rl = createInterface({
 })
 
 const keyMap = {
-  'q': () => quit(),
-  'r': () => {
-    stack = []
-    lastValid = ''
-    console.log('reset stack')
+  'c': iface => {
+    console.log('\x1Bc\x1b[3J')
+    iface.clearLine(0)
+    console.log(`\n${x1b('cleared screen', 11)}\n${commandsMsg}\n`)
+    iface.prompt()
   },
-  'h': () => console.log('help')
+  'h': () => console.log('help'),
+  'q': () => quit(),
+  'r': iface => {
+    stack = []
+    iface.clearLine(0)
+    console.log(`\n${x1b('reset stack', 11)}\n${commandsMsg}\n`)
+    iface.prompt()
+  }
 }
 
 rl.input.on('keypress', (char, props) => {
-  process.stdin.pause()
-  if (keyMap[props.name]) keyMap[props.name]()
-  process.stdin.resume()
+  if (keyMap[props.name]) keyMap[props.name](rl)
 })
 
 rl.on('line', line => {
-  if (line.length) {
-    const result = evaluate(line)
-    console.log(`${cursor}${x1b(lastValid, 4)}`)
-  }
+  if (line.length) evaluate(line)
   rl.prompt()
 })
 
 process.on('exit', () => {
-  rl.close()
+  // rl.close()
   console.log(x1b('\nquit', 10))
 })
 
