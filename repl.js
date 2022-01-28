18715c102c445f69314739ea1f5d99baeee70d9b
ixc7 <isaacxpreston7@gmail.com>
2022-01-28 15:42:40 -0500

    add verbose option 1

--- repl.js
+++ repl.js
@@ -4,6 +4,9 @@ const { createInterface } = require('readline')
 
 let stack = []
 
+// TODO
+let verbose = false
+
 const quit = (code = 0) => process.exit(code)
 const validNum = n => !isNaN(parseFloat(n)) && isFinite(n)
 const operators = {
@@ -19,40 +22,53 @@ const x1b = (msg, color = 1) => `\x1b[1m\x1b[38;5;${color}m${msg}\x1b[0m`
 const commandsMsg = `commands: ${x1b('q', 49)}(uit), ${x1b('r', 124)}(eset), ${x1b('c', 46)}(lear), ${x1b('h', 57)}(elp)`
 const initMsg = `\n${divider}\n${x1b('rpn calc v0.0.1', 128)}\n${divider}\n${commandsMsg}\n\n`
 
-const error = (msg = 'error') => console.log(x1b(msg))
+const error = (msg = 'error', color = 124) => console.log(x1b(msg, color))
 
 const evaluate = input => {
   const expr = input.split(' ').filter(i => i.length && i !== ' ')
   const len = expr.length
+  verbose && console.log(x1b(`\nargs: ${len}`, 4))
 
   for (let i = 0; i < len; i += 1) {
+    verbose && console.log(`\n${x1b(`[${i + 1}/${len}]`, 20)} char: ${x1b(expr[i], 15)}, stack: ${x1b(JSON.stringify(stack), 15)}`)
+    
     const char = expr[i]
     const isNum = validNum(char)
     const isOpr = operators[char]
 
     if (!isNum && !isOpr) return error(`Invalid syntax: ${char}`)
-    if (isNum) stack.push(parseFloat(char))
+    
+    if (isNum) {
+      stack.push(parseFloat(char))
+      verbose && console.log(`pushed ${x1b(char, 33)}`)
+    }
 
     else if (isOpr) {
       if (stack.length > 1) {
+        // validate with a copy before changing
         const localStack = [...stack]
         const b = localStack.pop()
         const a = localStack.pop()
         const calculated = operators[char](a, b)
+                
         if (!validNum(calculated)) return error(`Illegal operation: ${a} ${char} ${b}`)
-        
+        verbose && console.log(`calculated ${a} ${char} ${b} = ${x1b(calculated, 33)}`)
+
         localStack.push(calculated)
-        stack = localStack  
+        stack = localStack 
       }
       else if (stack.length === 1) {
-        let msg = `${x1b('Stack has one item:', 8)} ${x1b(stack[0], 6)}`
-        if (i > 0) msg += `\ncould not evaluate ${x1b(len - i, 6)} out of ${x1b(len, 6)} arguments passed`
-        return error(msg)
+        let msg = `\nStack has one item: ${x1b(stack[0], 4)}`
+        if (i > 0) msg += `\ncould not evaluate ${x1b(len - i, 125)} out of ${x1b(len, 125)} arguments passed`
+        
+        // return error(msg, 57)
+        // return error(msg, 244)
+        return error(msg, 7)
       }
-      else if (stack.length < 1) return error(x1b('Stack is empty', 8))
+      else if (stack.length < 1) return error('Stack is empty')
     }
   }
-
+  verbose && console.log(x1b('completed with no errors\n', 77))
   return console.log(`${cursor}${x1b(stack, 4)}`)
 }
 
@@ -64,8 +80,8 @@ const rl = createInterface({
 
 const keyMap = {
   'c': iface => {
-    console.log('\x1Bc\x1b[3J')
     iface.clearLine(0)
+    console.log('\x1Bc\x1b[3J')
     console.log(`\n${x1b('cleared screen', 11)}\n${commandsMsg}\n`)
     iface.prompt()
   },
@@ -80,7 +96,8 @@ const keyMap = {
 }
 
 rl.input.on('keypress', (char, props) => {
-  if (keyMap[props.name]) keyMap[props.name](rl)
+  // dont override ctrl-c behaviour
+  if (keyMap[props.name] && !props.ctrl) keyMap[props.name](rl)
 })
 
 rl.on('line', line => {
@@ -89,7 +106,6 @@ rl.on('line', line => {
 })
 
 process.on('exit', () => {
-  // rl.close()
   console.log(x1b('\nquit', 10))
 })
 
