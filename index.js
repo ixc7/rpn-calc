6df58f2e744c8e7c7ed5a1e2036f9a6132f78f76
ixc7 <isaacxpreston7@gmail.com>
2022-01-27 21:02:52 -0500

    add test.js, layout flags/options

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
 // TODO getopts
-// --interactive, -i
+// 
+// invoked with ...
+// (no args)              = repl
+// rpn 4 4 +              = once
+// echo "4 4 +" | rpn     = once
+// 
 // --help, -h
-// --help-all, (w/ examples? 'man RPN'?)
-// (maybe) return array/closest result before hitting an error?
-// (maybe) specify whether to return ...
-//         str: formatted str
-//         num: stack[0]
-//         both: { str, num }
+//   show this message and exit
+// 
+// --ignore-errors -i
+//   return the array on error
+// 
+// --format -f
+//   str =      'result: n'
+//   num =           number
+//   both =    { str, num }
+// 
+// --trace -t
+//   show step-by-step what is being evaluated
+//   ex: https://www.free-online-calculator-use.com/postfix-evaluator.html
+//
+// --examples -e
+//   show examples
 
 const _B = '\x1b[1m'
 const B_ = '\x1b[0m'
+
+const helpMsg = `
+${_B}rpn${B_} - evaluates postfix expressions
+usage: ${_B}rpn${B_} <exp>
+`
 
 const operators = {
   "+": (a, b) => a + b,
@@ -21,15 +41,10 @@ const operators = {
   "^": (a, b) => Math.pow(b, a)
 }
 
-const helpMsg = `
-  \r${_B}RPN${B_}: evaluates postfix expressions
-  \rusage: ${_B}RPN${B_} <expression>
-`
-
 const quit = msg => {
   msg && console.log(`error: ${_B}${msg}${B_}`)
   console.log(helpMsg)
-  process.exit(0)
+  process.exit(1)
 }
 
 const calculate = expr => {
@@ -47,18 +62,17 @@ const calculate = expr => {
       const b = parseInt(stack.pop())
       let res = false
 
-      // TODO use .?.?.?.? whatever its called
       if (operators[current]) res = operators[current](a, b)
-
       !res && quit(errorMsg)
+
       stack.push(res)
     }
   }
 
   (stack.length > 1) && quit(errorMsg)
-  return `result: ${_B}${stack[0]}${B_}`
-  // || return the actual number if specified
+  return `result: ${_B}${stack[0]}${B_}`  
 }
 
+// TODO accept single string w/ args
 !((process.argv.slice(2)).length) && quit(`input cannot be empty`)
 console.log(calculate(process.argv.slice(2)))
---
+++ test.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const { fork } = require('child_process')
+
+const test = input => {
+  const child = fork('./index.js', input, { stdio: ['ignore', 'ignore', 'ignore', 'ipc'] })
+
+  const status = msg => {
+    const output = `test: "${input.join(' ')}" \x1b[1m${msg}\x1b[0m\n`
+    console.log(output)
+    return output
+  }
+
+  child.on('exit', code => {
+    if (code === 0) return status('ok')
+    return status('not ok')
+  })
+}
+
+console.log('running tests\n')
+test([5, 8, '+'])
+test([5, 5, 5, 8, '+', '+', '-', 13, '+'])
+test([-3, -2, '*', 5, '+'])
+test([5, 9, 1, '-', '/'])
