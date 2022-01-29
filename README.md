dae65de475c3dce44fb70ce662d9c8b403fa18af
ixc7 <isaacxpreston7@gmail.com>
2022-01-29 09:09:58 -0500

    update README.md and package.json

---
+++ README.md
@@ -0,0 +1,24 @@
+## RPN Calculator CLI
+
+A Reverse Polish Notation/Postfix calculator for the command line.
+
+## Installation and Usage
+
+```
+npm i -g rpn-calc && rpn <expression>
+```
+
+or
+
+```
+npx rpn-calc <expression>
+```
+
+## Examples
+
+```
+rpn "2 2 +"
+rpn "100 5 / 2 +"
+rpn "8 9 * - 2"
+rpn "1 2 + 3 + 4 + 5 +"
+```
--- bin/index.js
+++ bin/index.js
@@ -3,51 +3,55 @@
 // TODO getopts
 // --interactive, -i
 // --help, -h
-// --help-all, (w/ examples, manpage ?)
+// --help-all, (w/ examples? 'man RPN'?)
+// (maybe) return array/closest result before hitting an error?
+// (maybe) specify whether to return ...
+//         str: formatted str
+//         num: stack[0]
+//         both: { str, num }
 
 const _B = '\x1b[1m'
 const B_ = '\x1b[0m'
+
 const helpMsg = `
   \r${_B}RPN${B_}: evaluates postfix expressions
   \rusage: ${_B}RPN${B_} <expression>
 `
 
-function quit (msg = false) {
+const quit = msg => {
   msg && console.log(`error: ${_B}${msg}${B_}`)
   console.log(helpMsg)
   process.exit(0)
 }
 
-function reversePolish(expr = false) {
+const run = expr => {
   const errorMsg = `invalid expression: ${_B}"${expr}"${B_}`  
-  if (!Array.isArray(expr)) quit(errorMsg)
-  
-  const stack =[]
-  for(let i = 0; i < expr.length; i += 1) {
-    
+  const stack = []
+
+  !Array.isArray(expr) && quit(errorMsg)
+
+  for(let i = 0; i < expr.length; i += 1) {    
     if (!isNaN(expr[i]) && isFinite(expr[i])) stack.push(expr[i])
     else {
       let res = false
       let a = stack.pop()
       let b = stack.pop()
-      
+
+      // TODO make faster?      
       if(expr[i] === "+") res = (parseInt(a) + parseInt(b))
       else if(expr[i] === "-") res = (parseInt(b) - parseInt(a))
       else if(expr[i] === "*") res = (parseInt(a) * parseInt(b))
       else if(expr[i] === "/") res = (parseInt(b) / parseInt(a))
       else if(expr[i] === "^") res = (Math.pow(parseInt(b), parseInt(a)))
 
-      if (!res) quit(errorMsg)
+      !res && quit(errorMsg)
       stack.push(res)
     }
   }
-  
+
   (stack.length > 1) && quit(errorMsg)
-  
-  // return stack[0]
-  // TODO option to specify whether to return str, num, { str, num }
   return `result: ${_B}${stack[0]}${B_}`
 }
 
 !((process.argv.slice(2)).length) && quit(`input cannot be empty`)
-console.log(reversePolish(process.argv.slice(2)))
+console.log(run(process.argv.slice(2)))
